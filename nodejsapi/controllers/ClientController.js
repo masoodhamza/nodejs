81b8137d350441130aa973bc67866af68f47e43f
Hamza Masood <masood.hamzaa@gmail.com>
2021-05-29 18:41:11 +0500

    added try catch

--- nodejsapi/controllers/ClientController.js
+++ nodejsapi/controllers/ClientController.js
@@ -15,60 +15,72 @@ async function getAllRestaurants(req, res, next) {
       });
     });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 }
 
 async function getRestaurantMenu(req, res, next) {
-  const { restaurantid } = req.params;
+  try {
+    const { restaurantid } = req.params;
 
-  let pool = sql.connect(config, function (error) {
-    if (error) console.log(error);
-
-    let sqlRequest = new sql.Request();
-    let sqlQuery = `EXEC GetRestaurantMenu ${restaurantid}`;
-    sqlRequest.query(sqlQuery, function (error, data) {
+    let pool = sql.connect(config, function (error) {
       if (error) console.log(error);
 
-      res.send(data.recordset);
+      let sqlRequest = new sql.Request();
+      let sqlQuery = `EXEC GetRestaurantMenu ${restaurantid}`;
+      sqlRequest.query(sqlQuery, function (error, data) {
+        if (error) console.log(error);
+
+        res.send(data.recordset);
+      });
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function createOrder(req, res, next) {
-  let { userid, itemid, addonid } = req.params;
+  try {
+    let { userid, itemid, addonid } = req.params;
 
-  if (!addonid) {
-    addonid = 0;
-  }
+    if (!addonid) {
+      addonid = 0;
+    }
 
-  let pool = sql.connect(config, function (error) {
-    if (error) console.log(error);
-
-    let sqlRequest = new sql.Request();
-    let sqlQuery = `EXEC CreateOrder ${userid}, ${itemid}, ${addonid}`;
-    sqlRequest.query(sqlQuery, function (error, data) {
+    let pool = sql.connect(config, function (error) {
       if (error) console.log(error);
 
-      res.send(data.recordset);
+      let sqlRequest = new sql.Request();
+      let sqlQuery = `EXEC CreateOrder ${userid}, ${itemid}, ${addonid}`;
+      sqlRequest.query(sqlQuery, function (error, data) {
+        if (error) console.log(error);
+
+        res.send(data.recordset);
+      });
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getOrderDetail(req, res, next) {
-  const { orderid } = req.params;
+  try {
+    const { orderid } = req.params;
 
-  let pool = sql.connect(config, function (error) {
-    if (error) console.log(error);
-
-    const sqlRequest = new sql.Request();
-    const sqlQuery = `EXEC GetOrderDetail ${orderid}`;
-    sqlRequest.query(sqlQuery, function (error, data) {
+    let pool = sql.connect(config, function (error) {
       if (error) console.log(error);
 
-      res.send(data.recordset);
+      const sqlRequest = new sql.Request();
+      const sqlQuery = `EXEC GetOrderDetail ${orderid}`;
+      sqlRequest.query(sqlQuery, function (error, data) {
+        if (error) console.log(error);
+
+        res.send(data.recordset);
+      });
     });
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports = {
